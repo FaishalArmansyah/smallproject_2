c0d10c137ccd922349ad15ef25db68e881f979aa
faishalarmansyah <faishal@linuxmail.org>
2022-06-10 16:20:48 +0700

    update sh files

--- appserver.sh
+++ appserver.sh
@@ -10,18 +10,16 @@ sudo a2enmod php7.4
 sudo systemctl restart apache2
 
 echo "===== Step 3 - Download App ====="
-sudo cp -r smallproject_2/sosial-media /var/www/
-sudo cp smallproject_2/sosial-media/sosmed.conf /etc/apache2/sites-available/
-sudo cp smallproject_2/sosial-media/servername.conf /etc/apache2/conf-available/
+sudo cp -r /smallproject_2/sosial-media /var/www/
+sudo cp /smallproject_2/sosial-media/sosmed.conf /etc/apache2/sites-available/
+sudo cp /smallproject_2/sosial-media/servername.conf /etc/apache2/conf-available/
 sudo a2enconf servername.conf && sudo a2ensite sosmed.conf && sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
 
 echo "===== 4. create & restore DB MySQL ====="
-mysql -h dbserver.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com -u root -p"Password.123!" -e "create database dbsosmed;"
-mysql -h dbserver.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com -u root -p"Password.123!" -e "grant all privileges on dbsosmed.* to 'devopscilsy'@'%' identified by '1234567890';"
-mysql -h dbserver.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com -u root -p"Password.123!" -e "flush privileges;"
-
-echo "===== 5. Restore DB ====="
-mysql -h dbserver.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com -u root -p"Password.123!" dbsosmed < smallproject_2/sosial-media/dump.sql
+mysql -h isi-dbserver -u root -p"isi-password" -e "create database dbsosmed;"
+mysql -h isi-dbserver -u root -p"isi-password" -e "grant all privileges on dbsosmed.* to 'isi-dbuser'@'%' identified by 'isi-dbpassword';"
+mysql -h isi-dbserver -u root -p"isi-password" -e "flush privileges;"
+mysql -h isi-dbserver -u isi-dbuser -p"isi-dbpassword" dbsosmed < /smallproject_2/sosial-media/dump.sql
 
 echo "===== Completed =====" 
--- server.sh
+++ server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 git clone https://github.com/FaishalArmansyah/smallproject_2.git
-sudo chmod 500 smallproject_2/appserver.sh
+sed -i 's/isi-password/Password.123!/g' /smallproject_2/appserver.sh
+sed -i 's/isi-dbserver/dbserver.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com/g' /smallproject_2/appserver.sh
+sed -i 's/isi-dbuser/devopscilsy/g' /smallproject_2/appserver.sh
+sed -i 's/isi-dbpassword/1234567890/g' /smallproject_2/appserver.sh
+sudo chmod 500 /smallproject_2/appserver.sh
 /bin/bash smallproject_2/appserver.sh
