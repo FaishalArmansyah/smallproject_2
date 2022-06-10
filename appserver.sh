e1ab790f46269b81275abfb8b3bb231a89c1ebd6
faishalarmansyah <faishal@linuxmail.org>
2022-06-11 00:38:25 +0700

    update sh files

--- appserver.sh
+++ appserver.sh
@@ -9,11 +9,21 @@ sudo apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli
 sudo a2enmod php7.4
 sudo systemctl restart apache2
 
-echo "===== Step 3 - Download App ====="
-sudo cp -r smallproject_2/sosial-media /var/www/
+echo "===== Step 2 - Install s3fs ====="
+sudo apt-get install -y automake autotools-dev fuse g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
+git clone https://github.com/s3fs-fuse/s3fs-fuse.git
+cd s3fs-fuse
+sudo ./autogen.sh
+sudo ./configure --prefix=/usr --with-openssl
+sudo make
+sudo make install
+echo access-key:secret-key > /etc/passwd-s3fs
+sudo chmod 640 /etc/passwd-s3fs
+sudo mkdir /var/www/sosial-media
+sudo s3fs sosmed-s3 /var/www/sosial-media --o passwd_file=/etc/passwd-s3fs -o url=https://s3.ap-southeast-1.amazonaws.com/ -ouid=1001,gid=1001,allow_other
 sed -i 's/localhost/isi-dbserver/g' /var/www/sosial-media/config.php
-sudo cp smallproject_2/sosial-media/sosmed.conf /etc/apache2/sites-available/
-sudo cp smallproject_2/sosial-media/servername.conf /etc/apache2/conf-available/
+sudo cp /var/www/sosial-media/sosmed.conf /etc/apache2/sites-available/
+sudo cp /var/www/sosial-media/servername.conf /etc/apache2/conf-available/
 sudo a2enconf servername.conf && sudo a2ensite sosmed.conf && sudo a2dissite 000-default.conf
 sudo systemctl restart apache2
 
@@ -21,6 +31,6 @@ echo "===== 4. create & restore DB MySQL ====="
 mysql -h isi-dbserver -u root -p"isi-password" -e "create database dbsosmed;"
 mysql -h isi-dbserver -u root -p"isi-password" -e "grant all privileges on dbsosmed.* to 'isi-dbuser'@'%' identified by 'isi-dbpassword';"
 mysql -h isi-dbserver -u root -p"isi-password" -e "flush privileges;"
-mysql -h isi-dbserver -u isi-dbuser -p"isi-dbpassword" dbsosmed < /smallproject_2/sosial-media/dump.sql
+mysql -h isi-dbserver -u isi-dbuser -p"isi-dbpassword" dbsosmed < /var/www/sosial-media/dump.sql
 
 echo "===== Completed =====" 
--- server.sh
+++ server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 git clone https://github.com/FaishalArmansyah/smallproject_2.git
-sed -i 's/isi-password/Password.123!/g' smallproject_2/appserver.sh
+sed -i 's/access-key:secret-key/AKIAR33JBTGZEXLKM3GQ:kjMKm/mR5PTQEuqh0bl8n0E7x7IQAU9phS+2g6sS/g' smallproject_2/appserver.sh
 sed -i 's/isi-dbserver/db-server.cir7gtvctrcn.ap-southeast-1.rds.amazonaws.com/g' smallproject_2/appserver.sh
+sed -i 's/isi-password/Password.123!/g' smallproject_2/appserver.sh
 sed -i 's/isi-dbuser/devopscilsy/g' smallproject_2/appserver.sh
 sed -i 's/isi-dbpassword/1234567890/g' smallproject_2/appserver.sh
 sudo chmod 500 smallproject_2/appserver.sh
